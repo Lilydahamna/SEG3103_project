04b056da562850991fea506d75cc89ea387ce5dd
Lilydahamna <liliadahamna2000@gmail.com>
2022-10-31 15:16:07 -0400

    created layout for CourseEditor

---
+++ app/src/main/java/com/seg2105/termprojectgroup21/CourseEditor.java
@@ -0,0 +1,14 @@
+package com.seg2105.termprojectgroup21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_editor);
+    }
+}
