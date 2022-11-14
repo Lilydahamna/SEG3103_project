54ae3db665b16a1aeb8ed49c627aee68fbb0ecad
Lilydahamna <liliadahamna2000@gmail.com>
2022-11-13 21:09:46 -0500

    made list of courses display again after clearing up search fields

--- app/src/main/java/com/seg2105/termprojectgroup21/CourseManager.java
+++ app/src/main/java/com/seg2105/termprojectgroup21/CourseManager.java
@@ -12,6 +12,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -72,6 +74,43 @@ public class CourseManager extends AppCompatActivity implements CourseAdapter.on
         if(sharedPref.getString("role", "").equals("Admin")){
             add = createButton(R.string.add);
         }
+
+        inputCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString().length() == 0 && inputName.getText().toString().length() == 0){
+                    courseAdapter.filterList(courses);
+                }
+            }
+        });
+        inputName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString().length() == 0 && inputCode.getText().toString().length() == 0){
+                    courseAdapter.filterList(courses);
+                }
+            }
+        });
 
 
     }
